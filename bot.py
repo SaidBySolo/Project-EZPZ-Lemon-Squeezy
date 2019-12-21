8c8125e140628f7cf507ce4cef9307279e33fc8d
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2019-12-21 01:02:18 +0000

    봇 기초 소스
    기본적인 틀

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+import datetime
+import random
+
+client = discord.Client()
+
+token = "Token"
+
+
+@client.event
+async def on_ready():
+    # Status message
+    print("login.... ")
+    print(client.user.name)
+    print(client.user.id)
+    print("======================")
+    
+@client.event
+async def on_message(message):
+    if message.content == ("안녕?"):
+        await message.channel.send("안녕! :smile:")
+
+client.run(token)
