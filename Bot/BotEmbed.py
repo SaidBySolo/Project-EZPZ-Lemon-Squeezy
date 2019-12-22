31fdd41341f3bd0fb8e59be48dbbb7838b387d73
SaBin1151 <hyunmin203401@gmail.com>
2019-12-22 15:11:02 +0900

    임베드파일 추가 및 임포트 코드 수정

---
+++ Bot/BotEmbed.py
@@ -0,0 +1,23 @@
+import datetime
+import discord
+
+now = datetime.datetime.now()
+
+Sabin = discord.Embed(title="Sabin", description="This Embed is a Profile of Sabin", color=0x569271)
+Sabin.set_author(name="Name : ")
+Sabin.set_thumbnail(url="https://imgur.com/j0yodUO.png")
+Sabin.add_field(name="Position", value="Leader Developer", inline=False)
+Sabin.add_field(name="Status", value=":tired_face:", inline=False)
+Sabin.add_field(name="Contact Information", value="Discord : Sabin#9478", inline=False)
+Sabin.set_footer(text="Don't call me...." + str(now.year) + "년 " + str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+
+#help embed setting
+Helps=discord.Embed(title="Bot", color=0xffff80)
+Helps.set_author(name="EZPZ Lemon Squeezy", icon_url="https://imgur.com/j0yodUO.png")
+Helps.set_thumbnail(url="https://imgur.com/L4YDwVk.png")
+Helps.add_field(name="봇 이름", value="???", inline=False)
+Helps.add_field(name="메인개발진", value="Main Developer : Sabin, SaidBySolo ", inline=False)
+Helps.add_field(name="개발진", value="Developer : Unoh03", inline=False)
+Helps.add_field(name="명령어", value="!도움말 : 도움말을 보여줍니다.\n안녕 : 인사해줍니다. 착해요.\n!제작현황 : 제작 현황이 나옵니다. 근데 쓸데없어요.\n!내정보 : 자신의 프로필이 나옵니다. 업데이트는 느려요.", inline=False)
+Helps.add_field(name="연락처", value="Sabin : Sabin#9478 (많이 연락하지 마세요....)\nSaidBySolo : 라고솔로가말했습니다#1234", inline=False)
+Helps.set_footer(text="ver 1.0.0    제작 - Sabin")
--- Bot/bot.py
+++ Bot/bot.py
@@ -1,21 +1,52 @@
 import discord
-from discord.ext import commands
+import datetime
+import random
+import BotEmbed
+
+client = discord.Client()
 
 token = "NjU3NjA0NDA3MDIxNjY2MzA0.XfzoCQ.3OBZ_60-CcPyaZcC-uGW9SLHOIo"
 
-#command_prefix
-bot = commands.Bot(command_prefix='!')
 
-@bot.event        
-        #Login
+@client.event
 async def on_ready():
-    print("login.... ")
-    print(bot.user.name)
-    print(bot.user.id)
+    # login
+    print("다음으로 로그인합니다 : ")
+    print(client.user.name)
+    print(client.user.id)
     print("======================")
 
-@bot.command()
-async def test(ctx):
-    await ctx.send('OK')
+    # 상태메시지 표시
+    #game0 = discord.Game("")
+    #await client.change_presence(status=discord.Status.online, activity=game0)
 
-bot.run(token)
+@client.event
+async def on_message(message):
+
+    if message.author.bot:
+        return None
+    if message.content == ("안녕"):
+        await message.channel.send("안녕! :smile:")
+
+    if message.content == ("!도움말"):
+        await message.channel.send(embed = BotEmbed.Helps)
+
+    if message.content == ("!제작현황"):
+        await message.channel.send("제작중")
+
+    #user id variable
+    Sabin_id = 406430032546889730
+    Huggy_id = 263643715401285632
+    SBS_id = 345265069132742657
+    Bliz_id = 268334477720289281
+    Decfcone1_id = 315373718568173568
+
+    if message.content == "!내정보":
+        if message.author.id == Sabin_id:
+            await message.channel.send(embed = BotEmbed.Sabin)
+
+    if message.content == "!내정보":
+        if message.author.id == SBS_id:
+            await message.channel.send(embed = BotEmbed.SaidBySolo)
+
+client.run(token)
