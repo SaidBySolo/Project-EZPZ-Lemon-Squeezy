9a4a99c2291d572a9ca437bcfc67b9ac774b1eaa
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-02-01 16:04:06 +0000

    프리픽스 작업 완료
    이외에 코드가독성 높임

--- Bot/BotEmbed.py
+++ Bot/BotEmbed.py
@@ -19,7 +19,7 @@ SaidBySolo.set_thumbnail(url="https://imgur.com/YBZEvFq.png")
 SaidBySolo.add_field(name="Position", value="Leader Developer", inline=False)
 SaidBySolo.add_field(name="Status", value=":thinking:", inline=False)
 SaidBySolo.add_field(name="Contact Information", value="Discord : 라고솔로가말했습니다#1234", inline=False)
-SaidBySolo.set_footer(text="Do U Wanna MAGIC TOUCH....?" + str(now.year) + "년 " + str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
+SaidBySolo.set_footer(text="Do U Wanna MAGIC TOUCH....? " + str(now.year) + "년 " + str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second))
 
 #Bliz Embed setting
 Bliz = discord.Embed(title="Bliz", description="This Embed is a Profile of bliz", color=0x569271)
@@ -51,4 +51,4 @@ Helps.add_field(name="메인개발진", value="Main Developer : Sabin, SaidBySol
 Helps.add_field(name="개발진", value="Developer : Unoh03", inline=False)
 Helps.add_field(name="명령어", value="!도움말 : 도움말을 보여줍니다.\n안녕 : 인사해줍니다. 착해요.\n!제작현황 : 제작 현황이 나옵니다. 근데 쓸데없어요.\n!내정보 : 자신의 프로필이 나옵니다. 업데이트는 느려요.", inline=False)
 Helps.add_field(name="연락처", value="Sabin : Sabin#9478 (많이 연락하지 마세요....)\nSaidBySolo : 라고솔로가말했습니다#1234", inline=False)
-Helps.set_footer(text="ver 1.0.0    제작 - Sabin")
+Helps.set_footer(text="ver 1.0.1    제작 - Sabin")
--- Bot/bot.py
+++ Bot/bot.py
@@ -3,18 +3,15 @@ import datetime
 import random
 import BotEmbed
 import botToken
-#from discord.ext import commands
+from discord.ext import commands
 
-#prefix(무기한연장)
-#bot = commands.Bot(command_prefix='!')
-
-#Client
-client = discord.Client()
+#prefix
+bot = commands.Bot(command_prefix='!')
 
 #paste token
 token = "NjU3NjA0NDA3MDIxNjY2MzA0.Xf-JVw.Krgxt54ycfRKhRs0oQ5vSf7PLwI"
 
-@client.event
+@bot.event
 async def on_ready():
     # login
     print("Login.. : ")
@@ -22,58 +19,29 @@ async def on_ready():
     print(bot.user.id)
     print("======================")
 
-    # Status message
-    game = discord.Game("v.1.0.0, !도움말")
-    await client.change_presence(status=discord.Status.online, activity=game)
+    # Status
+    game = discord.Game("v.1.0.1, !도움말")
+    await bot.change_presence(status=discord.Status.online, activity=game)
 
-
-    #need Prefix work.
     #commands
-@client.event
-async def on_message(message):
+@bot.command()
+async def 도움말(ctx):
+    await ctx.send(embed = BotEmbed.Helps)
 
-    if message.author.bot:
-        return None
-    if message.content == ("안녕"):
-        await message.channel.send("안녕! :smile:")
+async def 제작현황(ctx):
+    await ctx.send("제작중")
 
-    if message.content == ("!도움말"):
-        await message.channel.send(embed = BotEmbed.Helps)
-
-    if message.content == ("!제작현황"):
-        await message.channel.send("제작중")
-
-    #user id variable
-    Sabin_id = 406430032546889730
-    Huggy_id = 263643715401285632
-    SBS_id = 345265069132742657
-    Bliz_id = 268334477720289281
-    Decfcone1_id = 315373718568173568
-    JH_id = 587245907029000205
-
-    #my information
-    if message.content == "!내정보":
-        if message.author.id == Sabin_id:
-            await message.channel.send(embed = BotEmbed.Sabin)
-
-    if message.content == "!내정보":
-        if message.author.id == SBS_id:
-            await message.channel.send(embed = BotEmbed.SaidBySolo)
-
-    if message.content == "!내정보":
-        if message.author.id == Huggy_id:
-            await message.channel.send(embed = BotEmbed.Huggy)
-
-    if message.content == "!내정보":
-        if message.author.id == Bliz_id:
-            await message.channel.send(embed = BotEmbed.Bliz)
-
-    if message.content == "!내정보":
-        if message.author.id == Decfcone1_id:
-            await message.channel.send(embed = BotEmbed.Decfcone1)
-
-    if message.content == "!내정보":
-        if message.author.id == JH_id:
-            await message.channel.send(embed = BotEmbed.JH)
-
-client.run(token)
+@bot.command(name = "My information")
+async def 내정보(ctx):
+    react_embed = (ctx.author.id)
+    if react_embed is not None: 
+        if react_embed == 406430032546889730:
+            await ctx.send(embed = BotEmbed.Sabin)
+        elif react_embed == 345265069132742657:
+                await ctx.send(embed = BotEmbed.SaidBySolo)
+        else:
+                await ctx.send("임베드가 존재하지않습니다. 관리자에게 문의하여주세요")
+        
+            
+       
+bot.run(token)
