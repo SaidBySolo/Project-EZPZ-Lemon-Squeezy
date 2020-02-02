b871baa5576b3ad07cd7126072b9d15420d351e6
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-02-02 16:12:55 +0000

    반응추가
    투표 기능 10프로완료

--- Bot/bot.py
+++ Bot/bot.py
@@ -2,6 +2,7 @@ import discord
 import datetime
 import random
 import BotEmbed
+import botToken
 import userdict
 from discord.ext import commands
 
@@ -26,13 +27,13 @@ async def on_ready():
     # Status
     game = discord.Game("v.1.0.1, !도움말")
     await bot.change_presence(status=discord.Status.online, activity=game)
-
+ 
 #command not found
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("명령어를 찾을수없습니다. !도움말을 참조하여 확인해주세요")
-
+    
 #commands
 @bot.command()
 async def 도움말(ctx):
@@ -50,18 +51,19 @@ async def ping(ctx):
 async def 핑(ctx):
     await ctx.send(":ping_pong:퐁! {0}".format(round(bot.latency, 1)))
 
-@bot.command(pass_context = True)
-async def 투표(message):
-    await message.send(embed = BotEmbed.Vote1)
-    await message.add_reaction('✅')
+@bot.command()
+async def 투표(ctx):
+    message = await ctx.send(embed = BotEmbed.Vote1)
+    for emoji in ('✅', '❎'):
+        await message.add_reaction(emoji)
 
 #Information command
 @bot.command()
 async def 내정보(ctx):
     react_embed = userdict.user_embed.get(ctx.author.id)
-    if react_embed is not None:
+    if react_embed is not None: 
         await ctx.send(embed = react_embed)
     else:
         await ctx.send("엠베드가 존재하지않슴니다. 관리자에게 문의해주세요")
-
+       
 bot.run(token)
