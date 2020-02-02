13911f9f9bb51c1f2eb8eab1e9d5a57bcecac104
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-02-02 10:35:31 +0000

    명령어 오류 출력,유저 임베드 딕셔너리
    이외에 코드 가독성높임

--- Bot/bot.py
+++ Bot/bot.py
@@ -3,14 +3,19 @@ import datetime
 import random
 import BotEmbed
 import botToken
+import userdict
 from discord.ext import commands
 
 #prefix
 bot = commands.Bot(command_prefix='!')
 
+#remove defalt help command
+bot.remove_command ('help')
+
 #paste token
 token = "NjU3NjA0NDA3MDIxNjY2MzA0.Xf-JVw.Krgxt54ycfRKhRs0oQ5vSf7PLwI"
 
+#login,status
 @bot.event
 async def on_ready():
     # login
@@ -22,28 +27,26 @@ async def on_ready():
     # Status
     game = discord.Game("v.1.0.1, !도움말")
     await bot.change_presence(status=discord.Status.online, activity=game)
-
-    #commands
+ 
+#command not found
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("명령어를 찾을수없습니다. !도움말을 참조하여 확인해주세요")
+    
+#commands
 @bot.command()
 async def 도움말(ctx):
     await ctx.send(embed = BotEmbed.Helps)
 
+@bot.command()
 async def 제작현황(ctx):
     await ctx.send("제작중")
-
-#User Embed
-user_embed = {
-406430032546889730: BotEmbed.Sabin,
-345265069132742657: BotEmbed.SaidBySolo,
-263643715401285632: BotEmbed.Huggy,
-268334477720289281: BotEmbed.Bliz,
-259286662117326848: BotEmbed.Unoh03,
-}
-
+    
 #Information command
 @bot.command()
 async def 내정보(ctx):
-    react_embed = user_embed.get(ctx.author.id)
+    react_embed = userdict.user_embed.get(ctx.author.id)
     if react_embed is not None: 
         await ctx.send(embed = react_embed)
     else:
---
+++ Bot/userdict.py
@@ -0,0 +1,11 @@
+import discord
+import BotEmbed
+
+#User Embed
+user_embed = {
+406430032546889730: BotEmbed.Sabin,
+345265069132742657: BotEmbed.SaidBySolo,
+263643715401285632: BotEmbed.Huggy,
+268334477720289281: BotEmbed.Bliz,
+259286662117326848: BotEmbed.Unoh03,
+}
