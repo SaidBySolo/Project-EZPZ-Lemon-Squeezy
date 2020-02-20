8f774499063124345e65bce2d6c6c557bd8fe46e
SaidBySolo <saidbysolo@gmail.com>
2020-02-20 16:51:27 +0000

    Update faq.py

--- Bot/cogs/faq.py
+++ Bot/cogs/faq.py
@@ -5,6 +5,11 @@ from discord.ext import commands
 class FAQ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.guild:
+            await message.channel.send("Test")
         
 def setup(bot):
     bot.add_cog(FAQ(bot))
