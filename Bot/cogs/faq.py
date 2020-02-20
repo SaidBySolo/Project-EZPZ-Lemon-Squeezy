a48685e4563381d87e78f7eb9a41bbb1a264810a
SaidBySolo <saidbysolo@gmail.com>
2020-02-20 18:28:12 +0000

    문의 보내기 구현완료

--- Bot/cogs/faq.py
+++ Bot/cogs/faq.py
@@ -16,7 +16,9 @@ class FAQ(commands.Cog):
             qtm.add_field(name="문의자", value=f"{message.author}", inline=False)
             qtm.add_field(name="문의내용", value=f"{message.content}", inline=False)
             qtm.set_footer(text=f"문의일:(미국시간기준) {str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분 {str(now.second)}초")
-            await message.channel.send(embed = qtm)
+            await message.channel.send("문의가 정상적으로 처리되었습니다.")
+            channel = self.bot.get_channel(680105593817661486)
+            await channel.send(embed = qtm)
         
 def setup(bot):
     bot.add_cog(FAQ(bot))
