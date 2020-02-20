c298aa3a1a4bce0b2498f0cb345a82168fb5fbae
SaidBySolo <saidbysolo@gmail.com>
2020-02-20 16:08:03 +0000

    Update faq.py

--- Bot/cogs/faq.py
+++ Bot/cogs/faq.py
@@ -5,13 +5,6 @@ from discord.ext import commands
 class FAQ(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    @commands.dm_only()
-    async def on_message(self, ctx, message):
-        if message.content.startswith("ping"):
-            await ctx.send(message.channel, "Pong")
-
-
+        
 def setup(bot):
     bot.add_cog(FAQ(bot))
