9cba8b529c49e060fbcb93a6f9f316f7e453fae8
SaidBySolo <saidbysolo@gmail.com>
2020-02-20 17:47:03 +0000

    문의 임베드준비

--- Bot/cogs/faq.py
+++ Bot/cogs/faq.py
@@ -1,3 +1,4 @@
+import datetime
 import discord
 from discord.ext import commands
 
@@ -9,7 +10,13 @@ class FAQ(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.guild and message.author.id != 657604407021666304:
-            await message.channel.send(f"{message.author.id},{message.author.name},{message.author}")
+        now = datetime.datetime.now()
+        qtm = discord.Embed(title="DID", description=f"{message.author.id}", color=0x572271)
+        qtm.set_thumbnail(url=message.author.avatar_url)
+        qtm.add_field(name="문의자", value=f"{message.author}", inline=False)
+        qtm.add_field(name="문의내용", value=f"{message.content}", inline=False)
+        qtm.set_footer(text=f"문의일:(미국시간기준) {str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분 {str(now.second)}초")
+            await message.channel.send(embed = qtm)
         
 def setup(bot):
     bot.add_cog(FAQ(bot))
