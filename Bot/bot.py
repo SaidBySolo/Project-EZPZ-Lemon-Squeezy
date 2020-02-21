19b31af88d4f0da760d21694f1d088931216fda7
SaidBySolo <saidbysolo@gmail.com>
2020-02-21 06:43:44 +0000

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -84,7 +84,7 @@ async def on_ready():
     print("======================")
 
     # Status
-    game = discord.Game("v.1.0.1, +도움말")
+    game = discord.Game("v.1.0.1, ?도움말")
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 bot.run(token)
