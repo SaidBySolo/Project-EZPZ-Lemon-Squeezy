e16027a495b50ed62519ddc53311b4a013d1c0f6
SaidBySolo <saidbysolo@gmail.com>
2020-02-21 13:45:20 +0000

    nsfw 부분 1차 보완

--- Bot/cogs/nsfw.py
+++ Bot/cogs/nsfw.py
@@ -8,7 +8,7 @@ class NSFW(commands.Cog):
         self.bot = bot
     
     @commands.command()
-    @commands.is_nsfw( ) 
+    @commands.is_nsfw() 
     async def 히요비넘버(self, ctx, index):
         try:
             index = int(index)
@@ -26,18 +26,26 @@ class NSFW(commands.Cog):
             if len(soup.findAll('tr')) == 5:
                 tags = [t.text for t in trs[:5]]
                 nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
+                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/info/{index}',  inline=False)
+                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
                 await ctx.send(embed = nsfwser)
             elif len(soup.findAll('tr')) == 4:
                 tags = [t.text for t in trs[:4]]
                 nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
+                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/info/{index}',  inline=False)
+                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
                 await ctx.send(embed = nsfwser)
             elif len(soup.findAll('tr')) == 3:
                 tags = [t.text for t in trs[:3]]
                 nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
+                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/info/{index}',  inline=False)
+                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
                 await ctx.send(embed = nsfwser)
             elif len(soup.findAll('tr')) == 2:
                 tags = [t.text for t in trs[:2]]
                 nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
+                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/info/{index}',  inline=False)
+                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
                 await ctx.send(embed = nsfwser)
         
     @commands.command()
