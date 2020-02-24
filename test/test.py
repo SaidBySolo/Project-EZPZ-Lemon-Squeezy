f1f826be237fd60834987bc4f77d2e90361faef7
SaidBySolo <saidbysolo@gmail.com>
2020-02-24 11:49:06 +0900

    travis 테스트

--- test/test.py
+++ test/test.py
@@ -1,6 +1,22 @@
-from ..Bot import bot
+import os
+import discord
+from discord.ext import commands
 
-def test_load_cog():
-    bot = bot.initialize()
-    failed_cogs = bot.load_cogs(bot)
-    assert(len(failed_cogs) == 0)
+bot = commands.Bot(command_prefix='&')
+
+#cogs import here
+initial_extensions = ['cogs.' + x[:-3] for x in os.listdir("Bot/cogs") if x[-3:] == ".py" if x[-3:] == ".py" and not x.startswith("__")]
+
+def test_load_cogs():
+	if __name__ == '__main__':
+		for extension in initial_extensions:
+			try:
+				bot.load_extension(extension)
+				print(f"{extension} 로드 성공.")
+				assert(len(extension) == 0)
+			except Exception as e:
+				print(f"{extension} 로드 실패")
+				raise e
+
+
+
