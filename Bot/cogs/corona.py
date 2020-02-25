0449f8b0b0ae676bd0050a1dc597c8a3a03e812b
SaidBySolo <saidbysolo@gmail.com>
2020-02-25 14:13:30 +0900

    지정해서 가져오게 문제가생길거같아서 방식을 바꿈

--- Bot/cogs/corona.py
+++ Bot/cogs/corona.py
@@ -12,10 +12,13 @@ class Corona(commands.Cog):
         response = requests.get('https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=코로나')
         readerhtml = response.text
         soup = BeautifulSoup(readerhtml, 'lxml')
-        checked = soup.find('div', class_='circle red level3').find('p', class_='txt').find('strong', class_='num').text
-        free = soup.find('div', class_='circle blue level2').find('p', class_='txt').find('strong', class_='num').text
-        checking = soup.find('div', class_='circle orange level5').find('p', class_='txt').find('strong', class_='num').text
-        die = soup.find('div', class_='circle black').find('p', class_='txt').find('strong', class_='num').text
+        data1 = soup.find('div', class_='graph_view')
+        data2 = data1.findAll('div', class_='box')
+        data3 = data1.findAll('div', class_='box bottom')
+        checked = data2[0].find('p', class_='txt').find('strong', class_='num').text
+        checking = data2[2].find('p', class_='txt').find('strong', class_='num').text
+        free = data3[0].find('p', class_='txt').find('strong', class_='num').text        
+        die = data3[1].find('p', class_='txt').find('strong', class_='num').text
         wasup = soup.find('div', class_='csp_notice_info').find('p').find_all(text=True, recursive=True)
         #===============================
         coembed = discord.Embed(color=0x192131, title='코로나현황', description =f'{wasup[1]}' )
