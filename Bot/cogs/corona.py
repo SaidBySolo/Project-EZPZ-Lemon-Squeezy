15a99487b9044950d538caeea6f9827a792b5484
SaidBySolo <saidbysolo@gmail.com>
2020-02-25 11:51:55 +0900

    코로나 인라인 수정

--- Bot/cogs/corona.py
+++ Bot/cogs/corona.py
@@ -16,13 +16,13 @@ class Corona(commands.Cog):
         free = soup.find('div', class_='circle blue level2').find('p', class_='txt').find('strong', class_='num').text
         checking = soup.find('div', class_='circle orange level5').find('p', class_='txt').find('strong', class_='num').text
         die = soup.find('div', class_='circle black').find('p', class_='txt').find('strong', class_='num').text
-        wasup = soup.find('div', class_='csp_notice_info').find('p').find_all(text=True, recursive=False)
+        wasup = soup.find('div', class_='csp_notice_info').find('p').find_all(text=False, recursive=False)
         #===============================
         coembed = discord.Embed(color=0x192131, title='코로나현황', description =f'{wasup[1]}' )
-        coembed.add_field(name="확진자", value=f'{checked}명', inline=True)
-        coembed.add_field(name="격리해제", value=f'{free}명', inline=True)
-        coembed.add_field(name="검사중", value=f'{checking}명', inline=True)
-        coembed.add_field(name="사망자", value=f'{die}명', inline=True)
+        coembed.add_field(name="확진자", value=f'{checked}명', inline=False)
+        coembed.add_field(name="격리해제", value=f'{free}명', inline=False)
+        coembed.add_field(name="검사중", value=f'{checking}명', inline=False)
+        coembed.add_field(name="사망자", value=f'{die}명', inline=False)
         await ctx.send(embed = coembed)
 
 def setup(bot):
