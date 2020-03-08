bfbf9f7d5a7985d6dcc44e77a888ae3a47f8162e
Ryu juheon <saidbysolo@gmail.com>
2020-03-08 07:39:23 +0000

    웹소켓 핑 추가,날씨 waitinfo BotEmbed에서불러옴

--- Bot/cogs/ping.py
+++ Bot/cogs/ping.py
@@ -12,7 +12,12 @@ class Ping(commands.Cog):
         t1 = time.perf_counter()
         async with ctx.typing():
             t2 = time.perf_counter()
-        await ctx.send(f":ping_pong: Pong! {(round((t2-t1)*1000))}ms")
+        pingingembed = discord.Embed(title="Pinging....", description = "Wait a Second....")
+        editpinging = await ctx.send(embed = pingingembed)
+        latency = round(self.bot.latency * 1000)        
+        msglatency = round((t2-t1)*1000)
+        pingembed = discord.Embed(color=0x218362, title=":ping_pong: Pong!", description = f"Web Soket Delay\n{latency}ms\n\nFinal delay(Up to chat)\n{msglatency}ms")
+        await editpinging.edit(embed=pingembed)
 
 def setup(bot):
     bot.add_cog(Ping(bot))
