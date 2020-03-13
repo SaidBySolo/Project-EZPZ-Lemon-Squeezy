16725e22ddf38f4b6743fec453e983c7eda26531
Ryu juheon <saidbysolo@gmail.com>
2020-03-13 18:20:04 +0000

    nsfw.기능 부분적으로 추가
    썸네일,링크 필요

--- Bot/cogs/nsfw.py
+++ Bot/cogs/nsfw.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import requests
+import asyncio
 from bs4 import BeautifulSoup  
 from .etc.botembed import BotEmbed  
 
@@ -8,9 +9,10 @@ class NSFW(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    #최적화 예정
     @commands.command()
     @commands.is_nsfw() 
-    async def 히요비넘버(self, ctx, index):
+    async def 히요비번호(self, ctx, index):
         try:
             index = int(index)
         except ValueError:
@@ -51,25 +53,85 @@ class NSFW(commands.Cog):
 
     @commands.is_nsfw()
     @commands.command()
-    async def 히요비검색(ctx, tag):
-        response = requests.get(f"https://hiyobi.me/search/{tag}")
-        readerhtml = response.text
-        soup = BeautifulSoup(readerhtml, 'lxml')
-        bigtitle = soup.findAll('div', class_='gallery-content row')
-        smalltitle = bigtitle[0].find('span')
-        esulttitle = smalltitle.find('b').text 
-        result = smalltitle.findAll('tr')   
-        tags = [t.text for t in result]
-        embed = discord.Embed(title = resulttitle, description ="\n".join(tags))
-        msgedit = await ctx.send(embed = embed)
-
+    async def 히요비검색(self, ctx, tag):
+        try:
+            waitinfo = await ctx.send(embed = BotEmbed.waitinfoembed)
+            response = requests.get(f"https://hiyobi.me/search/{tag}")
+            readerhtml = response.text
+            soup = BeautifulSoup(readerhtml, 'lxml')
+            bigresult = soup.findAll('div', class_='gallery-content row')
+            titleall = soup.findAll('b')
+            title = [str(index) +". " + t.text for index, t in enumerate(titleall, 1)]
+            embed = discord.Embed(title = "검색된 정보입니다.", description ="\n".join(title))
+            msg = await waitinfo.edit(embed = embed)
+            await msg.add_reaction("🔍")
+            channel = ctx.channel
+            def check(reaction, user):
+                return user == ctx.author and str(reaction.emoji) == '🔍'
+            reaction, user = await self.bot.wait_for('reaction_add', check=check)
+            def check2(m):
+                return m.channel == channel 
+            try:
+                await ctx.send("30초이내에 번호를 입력하세요", delete_after=5)
+                response = await self.bot.wait_for('message', check=check2, timeout=30)
+            except asyncio.TimeoutError:
+                await ctx.send("시간 초과입니다.")
+            else:
+                if ctx.author == response.author:
+                    fetmsg = await ctx.fetch_message(response.id)
+                smalltitle = bigresult[int(fetmsg.content) - 1].find('span')
+                resulttitle = smalltitle.find('b').text 
+                result = smalltitle.findAll('tr')   
+                tags = [t.text for t in result]
+                embed = discord.Embed(title = resulttitle, description ="\n".join(tags))
+                await msg.edit(embed = embed)
+        except Exception as e:
+            embed = discord.Embed(title = "오류가발생한거같아요..:(", description = e)
+            embed.set_footer(text="다시 시도해보시고 지속될경우 봇에게DM 또는 개인적으로 컨택해주세요.")
+            await waitinfo.edit(embed = embed)
         
     @commands.command()
     async def 히요비리스트(self, ctx, num):
-        response = requests.get(f"https://hiyobi.me/list/{num}")
-        readerhtml = response.text
-        soup = BeautifulSoup(readerhtml, 'lxml')
-        soup.findAll('div', class_='gallery-content row')
+        try:
+            int(num)
+        except ValueError:
+            await waitinfo.edit("정수로 입력해주세요", delete_after=5)
+        else:
+            try:
+                waitinfo = await ctx.send(embed = BotEmbed.waitinfoembed)
+                response = requests.get(f"https://hiyobi.me/search/{num}")
+                readerhtml = response.text
+                soup = BeautifulSoup(readerhtml, 'lxml')
+                bigresult = soup.findAll('div', class_='gallery-content row')
+                titleall = soup.findAll('b')
+                title = [str(index) +". " + t.text for index, t in enumerate(titleall, 1)]
+                embed = discord.Embed(title = "검색된 정보입니다.", description ="\n".join(title))
+                msg = await waitinfo.edit(embed = embed)
+                await msg.add_reaction("🔍")
+                channel = ctx.channel
+                def check(reaction, user):
+                    return user == ctx.author and str(reaction.emoji) == '🔍'
+                reaction, user = await self.bot.wait_for('reaction_add', check=check)
+                def check2(m):
+                    return m.channel == channel 
+                try:
+                    await ctx.send("30초이내에 번호를 입력하세요", delete_after=5)
+                    response = await self.bot.wait_for('message', check=check2, timeout=30)
+                except asyncio.TimeoutError:
+                    await ctx.send("시간 초과입니다.")
+                else:
+                    if ctx.author == response.author:
+                        fetmsg = await ctx.fetch_message(response.id)
+                    smalltitle = bigresult[int(fetmsg.content) - 1].find('span')
+                    resulttitle = smalltitle.find('b').text 
+                    result = smalltitle.findAll('tr')   
+                    tags = [t.text for t in result]
+                    embed = discord.Embed(title = resulttitle, description ="\n".join(tags))
+                    await msg.edit(embed = embed)
+            except Exception as e:
+                embed = discord.Embed(title = "오류가발생한거같아요..:(", description = e)
+                embed.set_footer(text="다시 시도해보시고 지속될경우 봇에게DM 또는 개인적으로 컨택해주세요.")
+                await waitinfo.edit(embed = embed)
 
             
 def setup(bot):
