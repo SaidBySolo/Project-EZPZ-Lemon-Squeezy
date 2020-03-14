cf0484409481f3812de1fd317b68a8f2a5800343
Ryu juheon <saidbysolo@gmail.com>
2020-03-14 02:45:59 +0000

    링크및 썸네일 추가
    이외에 기존 번호 검색 최적화 완료

--- Bot/cogs/nsfw.py
+++ Bot/cogs/nsfw.py
@@ -22,34 +22,16 @@ class NSFW(commands.Cog):
         response = requests.get(f'https://hiyobi.me/info/{index}')
         readerhtml = response.text
         soup = BeautifulSoup(readerhtml, 'lxml')
-        table = soup.find('table')
-        trs = table.findAll('tr')
-        for title in soup.select('b'):
-            nsfwtitle = title.text
-            if len(soup.findAll('tr')) == 5:
-                tags = [t.text for t in trs[:5]]
-                nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
-                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/reader/{index}',  inline=False)
-                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
-                await waitinfo.edit(embed = nsfwser)
-            elif len(soup.findAll('tr')) == 4:
-                tags = [t.text for t in trs[:4]]
-                nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
-                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/reader/{index}',  inline=False)
-                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
-                await waitinfo.edit(embed = nsfwser)
-            elif len(soup.findAll('tr')) == 3:
-                tags = [t.text for t in trs[:3]]
-                nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
-                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/reader/{index}',  inline=False)
-                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
-                await waitinfo.edit(embed = nsfwser)
-            elif len(soup.findAll('tr')) == 2:
-                tags = [t.text for t in trs[:2]]
-                nsfwser = discord.Embed(color=0x569271, title=nsfwtitle, description = "\n".join(tags))
-                nsfwser.add_field(name="링크", value=f'https://hiyobi.me/reader/{index}',  inline=False)
-                nsfwser.set_thumbnail(url=f'https://hiyobi.me/tn/{index}.jpg')
-                await waitinfo.edit(embed = nsfwser)
+        bigresult = soup.find('div', class_='gallery-content row')
+        title = soup.find('b').text
+        smalltitle = bigresult.find('span')
+        link = bigresult.find('a')['href']
+        img = bigresult.find('img')['src']
+        result = smalltitle.findAll('tr')   
+        tags = [t.text for t in result]
+        embed = discord.Embed(url = link, title = title, description ="\n".join(tags))
+        embed.set_thumbnail(url=img)
+        await waitinfo.edit(embed = embed)
 
     @commands.is_nsfw()
     @commands.command()
@@ -80,10 +62,13 @@ class NSFW(commands.Cog):
                 if ctx.author == response.author:
                     fetmsg = await ctx.fetch_message(response.id)
                 smalltitle = bigresult[int(fetmsg.content) - 1].find('span')
+                link = bigresult[int(fetmsg.content) - 1].find('a')['href']
+                img = bigresult[int(fetmsg.content) - 1].find("img")["src"]
                 resulttitle = smalltitle.find('b').text 
                 result = smalltitle.findAll('tr')   
                 tags = [t.text for t in result]
-                embed = discord.Embed(title = resulttitle, description ="\n".join(tags))
+                embed = discord.Embed(url = link, title = resulttitle, description ="\n".join(tags))
+                embed.set_thumbnail(url=img)
                 await waitinfo.edit(embed = embed)
         except Exception as e:
             embed = discord.Embed(title = "오류가발생한거같아요..:(", description = e)
@@ -123,10 +108,13 @@ class NSFW(commands.Cog):
                     if ctx.author == response.author:
                         fetmsg = await ctx.fetch_message(response.id)
                     smalltitle = bigresult[int(fetmsg.content) - 1].find('span')
+                    link = bigresult[int(fetmsg.content) - 1].find('a')['href']
+                    img = bigresult[int(fetmsg.content) - 1].find("img")["src"]
                     resulttitle = smalltitle.find('b').text 
                     result = smalltitle.findAll('tr')   
                     tags = [t.text for t in result]
-                    embed = discord.Embed(title = resulttitle, description ="\n".join(tags))
+                    embed = discord.Embed(url = link, title = resulttitle, description ="\n".join(tags))
+                    embed.set_thumbnail(url=img)
                     await waitinfo.edit(embed = embed)
             except Exception as e:
                 embed = discord.Embed(title = "오류가발생한거같아요..:(", description = e)
