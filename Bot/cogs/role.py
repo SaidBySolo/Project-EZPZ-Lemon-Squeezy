ab7c91ddef85259bb9e80bd782b769888d3ac889
Ryu juheon <saidbysolo@gmail.com>
2020-03-15 16:34:55 +0000

    이모지 반응시 역할 얻는기능 추가

--- Bot/cogs/role.py
+++ Bot/cogs/role.py
@@ -4,6 +4,48 @@ from discord.ext import commands
 class Role(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        message_id = payload.message_id
+        if message_id == 688746995120209995:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
+        
+            if payload.emoji.name == '🔞':
+                role = discord.utils.get(guild.roles, name='NSFW')
+            else:
+                role = discord.utils.get(guild.roles, name=payload.emoji.name)
+            if role is not None:
+                member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.add_roles(role)
+                    print('add')
+                else:
+                    print("cant find user")
+            else:
+                print("do not have role")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        message_id = payload.message_id
+        if message_id == 688746995120209995:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
+        
+            if payload.emoji.name == '🔞':
+                role = discord.utils.get(guild.roles, name='NSFW')
+            else:
+                role = discord.utils.get(guild.roles, name=payload.emoji.name)
+            if role is not None:
+                member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.remove_roles(role)
+                    print('remove')
+                else:
+                    print("cant find user")
+            else:
+                print("do not have role")
 
 
 def setup(bot):
