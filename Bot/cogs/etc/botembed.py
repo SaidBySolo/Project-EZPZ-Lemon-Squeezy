e60e635b75725d6e90893943bf8155af38734a91
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-04-09 20:44:27 +0900

    가져오는중->요청하는중으로 변경

--- Bot/cogs/etc/botembed.py
+++ Bot/cogs/etc/botembed.py
@@ -40,7 +40,7 @@ class BotEmbed:
 
 
     #wait info
-    waitinfoembed = discord.Embed(title="서버로부터 가져오는중이에요!", description = "잠시만기다려주세요.....")
+    waitinfoembed = discord.Embed(title="서버로부터 요청하는중이에요!", description = "잠시만기다려주세요.....")
     waitinfoembed.set_footer(text="이 메세지가 계속뜨고 지속된다면 명령어를 맞게,없는정보를 요청하였는지확인하고\n 그래도 이문제가 계속될경우 봇에게 DM해주세요.")
 
     #dev embed setting
